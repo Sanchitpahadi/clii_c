601107830efeb5a248ae9b126f26454c2c0bcb39
sanc <sanchit.pahadi4u@gmail.com>
2026-08-24 18:40:10 +0000

    echo pwd ls mkdir

--- cli.cpp
+++ cli.cpp
@@ -1,42 +1,185 @@
+#include <sstream>
 #include<string>
 #include"cli.h"
 
-
-void CLI::run()
-{
-    string command;
-    cout << "Suprising Git Hub By pushing/start CLI in 1 AM" << endl;
-    cout << "************Devloper Mode**************" << endl;
-    while(true)
+    CLI::CLI()
     {
-        cout << "dev > ";
-        getline(cin,command);
-
-        if(command == "exit")
-        {
-            cout << "THanks for using my program \n";
-            break;
-        }
-        processCommand(command);
+        registerCommands();
     }
-}
+    void CLI::run()
+    {
+        string input;
+        cout << "************Devloper Mode**************" << endl;
+        while(true)
+        {
+            cout << "dev > ";
+            if(!getline(cin,input))
+            {
+                break;
+            }
 
-void CLI::processCommand(const string& command)
-{
+            if(input.empty())
+            {
+                continue;
+            }
+
+            if(input == "exit")
+            {
+                cout << "THanks for using my program \n";
+                break;
+            }
+            processCommand(input);
+        }
+    }
+
+    void CLI::registerCommands()
+    {
+        commands.push_back({"help", "show available command"});
+        commands.push_back({"status", "show CLI status"});
+        commands.push_back({"echo", "print the text"});
+        commands.push_back({"clear", "clear the screen"});
+        commands.push_back({"exit", "exit the program"});
+        commands.push_back({"ls", "list the directory inthe sys"});
+        commands.push_back({"mkdir", "makes the directory inside the file"});
+        commands.push_back({"pwd", "show the current directory"});
+
+    }
+
+    void CLI::processCommand(const string& input)
+    {
+        vector<string> args = parseCommand(input);
+
+        if(args.empty())
+        {}
+        string command = args[0];
+
         if(command == "help")
         {
-            cout << "Available Command \n";
-            cout << " help \n";
-            cout << " status \n";
-            cout << " exit \n";
+            commandHelp();
         }
         else if(command == "status")
         {
-            cout << " its running man \n";
+           commandStatus();
         }
-
+       else if(command == "clear")
+        {
+           commandClear();
+        }
+        else if(command == "echo")
+        {
+            commandEcho(args);
+        }
+        else if(command == "pwd")
+        {
+            commandPwd();
+        }
+        else if(command == "ls")
+        {
+            commandLs();
+        }
+        else if(command == "mkdir")
+        {
+            commandMkdir(args);
+        }
         else
         {
             cout << "check for help \n";
         }
-}
+    }
+   
+    void CLI::commandPwd()
+    {
+        cout << filesystem::current_path() << '\n';
+    }
+
+    void CLI::commandLs()
+    {
+        for(const auto& entry : filesystem::directory_iterator(filesystem::current_path()))
+        {
+            cout << entry.path().filename().string() << ' ';
+        }
+        cout << '\n';
+    } 
+
+     void CLI::commandMkdir(const vector<std::string>& args)
+    {
+        if(args.size() < 2)
+        {
+            cout << "Usage:  mkdir <directory> \n";
+
+            return;
+        }
+        try
+        {
+            if(filesystem::create_directory(args[1]))
+            {
+                cout <<"Directory created. \n";
+            }
+            else
+            {
+                cout <<"Directory already exist . \n";
+
+            }
+        }
+        catch(const filesystem::filesystem_error& error)
+        {
+            cout << error.what() << '\n';
+        }
+    } 
+    void CLI::commandClear()
+    {
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
+    }
+
+    void CLI::commandHelp()
+    {
+        cout << "Available Command \n";
+        for(const Command & command :commands)
+        {
+            cout << " " << command.name << "---" << command.description << "\n";
+        }
+
+        cout << "\n";
+    }
+
+    void CLI::commandStatus()
+    {
+        cout << " its running man \n";
+        cout << "Everything going good \n";
+    }
+
+    vector<string> CLI::parseCommand(const string& input)
+    {
+        vector<string> args;
+
+        stringstream stream(input);
+
+        string word;
+
+        while (stream >> word)
+        {
+            args.push_back(word);
+        }
+
+        return args;
+    }
+
+    void CLI::commandEcho(const vector<string> & args)
+    {   
+        for(size_t i = 1; i <args.size(); ++i)
+        {
+            cout << args[i];
+            
+            if(i +1 < args.size())
+            {
+                cout << " ";
+            }
+        }
+
+        cout << "\n";
+
+    }
--- cli.h
+++ cli.h
@@ -1,11 +1,39 @@
 #include<iostream>
+#include<vector>
+#include<filesystem>
 
 using namespace std;
 
+struct Command
+{
+    string name;
+    string description;
+};
 class CLI
 {
 public:
+    CLI();
     void run();
 private:
-    void processCommand(const string& command);
+
+    //filesytem 
+    void commandPwd();
+    void commandLs();
+    void commandMkdir(const vector<std::string>& args);
+
+    //others normal
+    void registerCommands(); // 
+    void processCommand(const string& input);
+    void commandClear();
+    void commandHelp(); // show the commands 
+    void commandStatus();
+    void commandEcho(const vector<string> & args);
+
+    vector<string> parseCommand(const string& input); // a whole line into words and store in vecotr
+
+    /*
+    its just a good if you write variables at bottom cause class is read from the bottom by 
+     compilers
+    */ 
+    vector<Command> commands; // eg. help status echo exit;
 };
